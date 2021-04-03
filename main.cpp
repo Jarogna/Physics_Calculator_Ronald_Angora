f5122b29fa91d8878f9a7a9cb8275239b3cb4e80
Jarogna <>
2021-04-03 14:51:32 +0000

    Added Menu Function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+// Function to display the menu
+void menu()
+{
+  cout << "1. Calculate Velocity\n";
+  cout << "2. Calculate Acceleration\n";
+  cout << "3. Calculate Motion\n";
+  cout << "4. Calculate Newtons 2nd Law\n";
+  cout << "5. Calculate Weight\n";
+  cout << "6. Calculate Momentum\n";
+  cout << "Press E or e to exit\n";
+}
+
+int main() 
+{
+
+
 }
