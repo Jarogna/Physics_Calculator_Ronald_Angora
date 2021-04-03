a8d3b7c4a5dd619e92ff3155de246e58a685672f
Jarogna <>
2021-04-03 15:04:44 +0000

    Added SubMenu Function

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 // Function to display the menu
 void menu()
 {
+  cout << "***Physics Calculator***\n";
+  cout << "Enter 1-6 to select formula or E to exit\n";
   cout << "1. Calculate Velocity\n";
   cout << "2. Calculate Acceleration\n";
   cout << "3. Calculate Motion\n";
@@ -11,6 +13,18 @@ void menu()
   cout << "6. Calculate Momentum\n";
   cout << "Press E or e to exit\n";
 }
+
+// Function to display the submenu
+void submenu()
+{
+  cout << "What do you want to solve for?\n";
+  cout << "1. Solve for Ma\n";
+  cout << "2. Solve for Ms\n";
+  cout << "3. Solve for Mv2\n";
+  cout << "4. Solve for Mv\n";
+  cout << "5. Go back to Main Menu\n";
+}
+
 
 int main() 
 {
